05f9b46322f43af816e333ab2f7b8c5285ef2710
DungDinhDungDinh <channhoibong1995@gmail.com>
2017-01-16 11:09:17 +0700

    ADD API LẤY LIST COMMENTS

--- server/server.js
+++ server/server.js
@@ -288,5 +288,49 @@ app.post('/comments', function(req, res) {
     
 });
 
+//Hàm tính khoảng cách
+var getDistanceFromLatLonInKm = function(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+//Lấy top 5 thingstodo
+app.get('/topthingstodo', function(req, res) {
+	ThingsToDo.find({
+    }).sort({_ratingCount: 1}).limit(5).select().exec(function(err, things) {
+        if (err) {
+            return res.status(404).send('Not found');
+            console.log('Failed!!');
+        } else {
+            res.status(200).send(things);
+            console.log(things);
+        }
+    });
+});
+
+//API Lấy List comment của ThingsToDo
+app.get('/comments', function(req, res) {
+	var _idThing= req.query._idThing;
+	Review.find({_thingsToDoID: _idThing
+    }).sort({_time: 1}).select().exec(function(err, reviews) {
+        if (err) {
+            return res.status(404).send('Not found');
+            console.log('Failed!!');
+        } else {
+            res.status(200).send(reviews);
+            console.log(reviews);
+        }
+    });
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port);

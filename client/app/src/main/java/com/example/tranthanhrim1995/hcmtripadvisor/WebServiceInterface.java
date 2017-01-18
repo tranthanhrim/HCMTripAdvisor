3c5d8db54511d122c3883118fc465d7fcf78ad33
RimTran <tranthanhrim1995@gmail.com>
2017-01-18 09:28:31 +0700

    Commit Web Service

--- client/app/src/main/java/com/example/tranthanhrim1995/hcmtripadvisor/WebServiceInterface.java
+++ client/app/src/main/java/com/example/tranthanhrim1995/hcmtripadvisor/WebServiceInterface.java
@@ -1,11 +1,17 @@
 package com.example.tranthanhrim1995.hcmtripadvisor;
 
+import com.example.tranthanhrim1995.hcmtripadvisor.Model.Comment;
+import com.example.tranthanhrim1995.hcmtripadvisor.Model.DetailImage;
 import com.example.tranthanhrim1995.hcmtripadvisor.Model.DetailThing;
 import com.example.tranthanhrim1995.hcmtripadvisor.Model.Thing;
+import com.example.tranthanhrim1995.hcmtripadvisor.Model.ThingDistance;
 import com.example.tranthanhrim1995.hcmtripadvisor.Model.User;
 import com.example.tranthanhrim1995.hcmtripadvisor.Model.response.EndPointResponse;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
@@ -13,6 +19,7 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by tranthanhrim1995 on 1/11/2017.
@@ -22,9 +29,40 @@ public interface WebServiceInterface {
     @GET("thingstodo")
     Call<List<Thing>> listThingsToDo();
 
-    @GET("details_id?detailSearch={id}")
-    Call<DetailThing> detailThing(@Path("id") String id);
+    @GET("thingstodo_type?typeSearch={typeOfThing}")
+    Call<List<Thing>> getlistThingsWithType(@Path("typeOfThing") String typeOfThing);
 
-    @POST("user")
+    @GET("thingstodo_destination")
+    Call<List<Thing>> getListDestination();
+
+    @GET("details_id")
+    Call<List<DetailThing>> detailThing(@Query("detailSearch") String detailSearch);
+
+    @GET("images")
+    Call<List<DetailImage>> detailImage(@Query("imageSearch") String imageSearch);
+
+    @POST("users")
     Call<EndPointResponse> createUser(@Body User user);
+
+    @POST("comments")
+    Call<EndPointResponse> postComment(@Body HashMap map);
+//    Call<EndPointResponse> postComment(@Path("_idUser") String idUser, @Path("_idThing") String idThing, @Path("_content") String content);
+
+    @POST("ratings")
+    Call<EndPointResponse> postRate(@Body HashMap map);
+
+    @GET("comments")
+    Call<List<Comment>> getComments(@Query("_idThing") String _idThing);
+
+    @GET("nearme")
+    Call<List<ThingDistance>> getListIdThingNearMe(@Query("lon") double lon, @Query("lat") double lat);
+
+    @GET("topthingstodo")
+    Call<List<Thing>> getListTopThingsToDo();
+
+    @GET("ratescounting")
+    Call<Map> getRateCounting(@Query("_idThing") String _idThing);
+
+    @GET("personrating")
+    Call<Map> getPersonalRating(@Query("_idUser") String _idUser, @Query("_idThing") String _idThing);
 }
